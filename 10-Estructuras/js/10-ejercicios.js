9ab6a7a7dc6daf09bcfb8ad1abccde183f22eed7
Mauricio Belmonte <maubr170295@gmail.com>
2026-01-07 22:45:14 -0700

    avances estructuras

---
+++ 10-Estructuras/js/10-ejercicios.js
@@ -0,0 +1,18 @@
+// Ejercicio 1 Sistema de calificaciones
+
+const calification  = 90;
+
+if(calificaion >= 90 && calificacion <= 100){
+    console.log('Excelente');
+}else if(calificacion >= 80){
+    console.log('Muy bien');
+}else if(calificacion >= 70){
+    console.log('Bien');
+}else if(calificacion >= 60){
+    console.log('Suficiente');
+}else{
+    console.log('Reprobado');
+}
+
+
+// Ejercicio 2 
